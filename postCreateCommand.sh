6f2ba600fce185cf711ee50310c7db964e89a541
Cam <cscarslake@protonmail.com>
2024-08-13 17:43:38 +0930

    update method of odoo dependency install

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -3,5 +3,6 @@
 echo "Installing odoo dependencies.."
 
 cd ./odoo
-sed -n -e '/^Depends:/,/^Pre/ s/ python3-\(.*\),/python3-\1/p' debian/control | sudo xargs apt-get install -y
+sudo ./setup/debinstall.sh
+
 
